eeaa290f065e16726a46c84c2cd5d85a0eb2b519
164031-Pannakorn <164031@wsk.ac.th>
2026-06-18 07:45:21 +0000

    pannakorn.py

---
+++ pannakorn.py
@@ -0,0 +1,10 @@
+print("Rectangle Area and Perimeter Calculator")
+
+width = float(input("Enter width: "))
+length = float(input("Enter length: "))
+
+area = width * length
+perimeter = 2 * (width + length)
+
+print("Area =", area)
+print("Perimeter =", perimeter)
